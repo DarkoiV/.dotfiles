9570839e26af902afa6cf962e597846c415d3c97
Maksymilian <makkob997@gmail.com>
2023-11-10 20:57:46 +0100

    Added sorting rules

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -7,9 +7,46 @@ local TREE_WINDOW_HEIGHT = math.ceil(math.min(100, EDITOR_HEIGHT / 1.5))
 local TREE_POS_Y = (EDITOR_HEIGHT - TREE_WINDOW_HEIGHT) * 0.4
 local TREE_POS_X = (EDITOR_WIDTH - TREE_WINDOW_WDITH) * 0.5
 
+local isSpecialFileName = function(name)
+    if string.sub(name, 1, 4) == "init" then
+        return true
+    end
+    return false
+end
+
+local sortSpecialNameFilesFirst = function(a, b)
+    if isSpecialFileName(a.name) and isSpecialFileName(b.name) then
+        return a.name < b.name
+    elseif isSpecialFileName(a.name) then
+        return true
+    elseif isSpecialFileName(b.name) then
+        return false
+    else
+        return a.name < b.name
+    end
+end
+
+local sortFiles = function(a, b)
+    if a.type == "directory" and b.type == "directory" then
+        return a.name < b.name
+    elseif a.type == "directory" then
+        return true
+    elseif b.type == "directory" then
+        return false
+    else
+        return sortSpecialNameFilesFirst(a, b)
+    end
+end
+
 tree.setup
 {
     hijack_cursor = true,
+    sort =
+    {
+        sorter = function(nodes)
+            table.sort(nodes, sortFiles)
+        end
+    },
     view =
     {
         float =
